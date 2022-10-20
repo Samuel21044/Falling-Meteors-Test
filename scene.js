bd091c892caac176b81f5a1c610f124204e87e1b
Samuel21044 <99492595+Samuel21044@users.noreply.github.com>
2022-10-20 11:02:10 -0500

    1 file modified

--- scene.js
+++ scene.js
@@ -9,7 +9,7 @@ export default class star{
     ctx.fillRect(this.x, this.y, this.size, this.size);
   }
   drawSun(ctx) {
-    ctx.fillStyle = 'white';
+    ctx.fillStyle = 'rgb(255, 218, 66, 0.95)';
     ctx.fillRect(this.x, this.y, this.size, this.size);
   }
 }
@@ -18,10 +18,16 @@ let stars = [];
 for(let i = 0; i < 100; i++) {
   stars.push(new star(Math.floor(Math.random() * 750), Math.floor(Math.random() * 640)));
 }
-
+let sun = [];
+for(let i = 0; i < 400; i++) {
+  sun.push(new star(Math.floor(Math.random() * 750) / i, Math.floor(Math.random() * 500) / i));
+}
 export function mainBackground(ctx) {
-  //do someting
+  //stars and sun
   for(let i = 0; i < stars.length; i++) {
     stars[i].draw(ctx);
+  }
+  for(let i = 0; i < stars.length; i++) {
+    sun[i].drawSun(ctx);
   }
 }
