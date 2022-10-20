5ef3b817270292aef676f81041e1f2f243189735
Samuel21044 <99492595+Samuel21044@users.noreply.github.com>
2022-10-19 22:16:34 -0500

    1 file modified

--- keyInput.js
+++ keyInput.js
@@ -3,12 +3,20 @@ export default class keyboard {
 
     document.addEventListener("keydown", event => {
       switch (event.keyCode) {
+        //movement
         case 37:
             player.moveLeft();
           break;
         case 39:
             player.moveRight();
           break;
+        case 65:
+            player.moveLeft();
+          break;
+        case 68:
+            player.moveRight();
+          break;
+        //starting a new game
         case 13:
             if(game.gamestate === 1) {
               game.Score = 0;
@@ -24,6 +32,12 @@ export default class keyboard {
             if(player.speed < 0) player.stop();
           break;
         case 39:
+            if(player.speed > 0) player.stop();
+          break;
+        case 65:
+            if(player.speed < 0) player.stop();
+          break;
+        case 68:
             if(player.speed > 0) player.stop();
           break;
       }
