e518d4cb155d2370054637de6ab7f50399cea4fa
Samuel21044 <99492595+Samuel21044@users.noreply.github.com>
2022-10-21 14:01:08 -0500

    1 file modified

--- fallingBlocks.js
+++ fallingBlocks.js
@@ -26,6 +26,7 @@ export default class fallingBlocks {
     //collide with player
     if(collisionDetection(2, 0, 0, this, player)) {
       game.gameOver = true;
+      player.stop();
 
       //timer
       setTimeout(() => {
