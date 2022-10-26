d6fdea9e104bd20145021e8f5578968a06fbcf56
Samuel21044 <99492595+Samuel21044@users.noreply.github.com>
2022-10-25 20:04:29 -0500

    1 file modified

--- game.js
+++ game.js
@@ -5,6 +5,11 @@ Add something that keeps track of the score in the top corner of smth
 Fix point problem
 
 
+
+For meteors decrease the width and height by a certain amount and draw the image in the center using  ---> this.x + this.w / 2
+And in drawing the image itself I manually add some stuff to have the collision smaller but the width and height the same
+I also do this with the rocket bc it is too big
+
 //important
 |||||||||||
 
