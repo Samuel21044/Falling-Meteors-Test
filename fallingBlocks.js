16db8a2d62333d50abfd7301f23ba2f617be60dc
Samuel21044 <99492595+Samuel21044@users.noreply.github.com>
2022-10-26 10:44:57 -0500

    1 file modified

--- fallingBlocks.js
+++ fallingBlocks.js
@@ -4,7 +4,7 @@ import { collisionDetection } from './collide.js';
 export default class fallingBlocks {
   constructor(x, y) {
     this.x = x; this.y = y;
-    this.w = 25; this.h = 50;
+    this.w = 25; this.h = 40;
 
     this.speed = 0; this.maxSpeed = 100;
     this.timerTillNextBlock = 50;
@@ -47,6 +47,6 @@ export default class fallingBlocks {
     }
   }
   draw(ctx) {
-    ctx.drawImage(document.getElementById('meteorOnFire'), this.x, this.y, this.w, this.h);
+    ctx.drawImage(document.getElementById('meteorOnFire'), this.x, this.y-10, this.w, this.h+10);
   }
 }
